f281620d26bfcca9ef94e6464ce3cfaecdb26057
0svaldojunior <osvaldo.rodrigues@acad.pucrs.br>
2020-03-25 15:52:09 -0300

    Backend finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Be-The-Hero
+# Be-The-Hero aplicação desenvolvida pela Semana OmniStack 11 da RocketSeat
+Técnologias:
+    - Back-end: node.js
+    - Front-end web: React
+    - Front-end mobile: React Native
---
+++ backend/src/comentarios.js
@@ -0,0 +1,36 @@
+/*
+    Rota / Recurso
+*/
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query Parms: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+  * Route Params Parâmetros utilizados para indentificar recursos
+  * Request Body: Corpo da requisiçào, utilizado para criar ou declarar recursos
+  */
+
+  /**
+   * Nodemon: 
+   * Step 1: npm install nodemon -D
+   * Step 2: package.json  - scripts - "start": "nodemon index.js"
+   */
+
+   /**
+    * SQL: MySQL, SQLite, PostgreeSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver: SELECT * FROM users
+     * Query Builder: table('users').select('*').where()
+     */
